8a901cc10d80e953b67b8f3d5438aeb353f29dec
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2023-02-16 21:02:22 +0530

    Time: 12 ms (69.06%), Space: 18.4 MB (78.02%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -8,6 +8,7 @@ public:
             if(nums[i] != nums[i+1])
                 nums[ans++] = nums[i];
         }
+//      As the last element will never get compared whether it is duplicate or not so add it directly at the end
         nums[ans++] = nums[nums.size()-1];
         return ans;
     }
