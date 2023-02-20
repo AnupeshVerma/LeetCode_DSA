94f721cf9a640e97912a055dc3511746aabd23dc
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2023-02-20 21:42:27 +0530

    Time: 0 ms (100.00%), Space: 12 MB (75.81%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+    // USing C++ STL
+    //next_permutation(nums.begin(), nums.end());
+
+    int ind =-1;
+
+    for(int i=nums.size()-2; i>=0; i--)
+        if(nums[i+1] > nums[i])
+        {
+            ind = i;
+            break;
+        }
+
+     for(int i=nums.size()-1; i>=ind && ind!=-1; i--)
+    {
+        if(nums[i] > nums[ind])
+        {
+           swap(nums[i], nums[ind]);
+           break;
+        }
+
+    }
+        reverse(nums.begin()+ind+1, nums.end());
+        
+            
     }
 };
