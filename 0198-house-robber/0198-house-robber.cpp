2b57c46b2732f623cbc5b8bbdbd4dc91b1bd8a11
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2023-06-04 02:25:34 +0530

    Time: 4 ms (29.61%), Space: 9.6 MB (6.96%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rec(vector<int>nums, int n, vector<int> &dp)
+    {
+        if(n<=0)
+            return 0;
+        if(dp[n] != -1)
+            return dp[n];
+        
+        return dp[n] = max(rec(nums, n-2, dp)+nums[n-1], rec(nums, n-1, dp));
+    }
+    
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>dp(n+1, -1);
+//         RECURSIVE + MEMO
+        return rec(nums, n, dp);
+    }
+};
