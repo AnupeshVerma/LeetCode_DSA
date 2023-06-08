2bf95f5801139d8613eb01c702da7e39804084fe
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2023-06-08 21:43:03 +0530

    Time: 10 ms (67.96%), Space: 10.1 MB (46.98%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -2,17 +2,28 @@ class Solution {
 public:
     int bottomUp(vector<vector<int>>&grid, vector<vector<int>>&dp, int m, int n)
     {
-        if(m<0 || n<0)
-            return 0;
-        return 1;
+        if(m==0 && n==0)
+            return grid[0][0];
+        
+        if(dp[m][n] != -1)
+            return dp[m][n];
+        
+        int up=INT_MAX, left=INT_MAX;
+        if(m>0) up = grid[m][n] + bottomUp(grid, dp, m-1, n);
+        if(n>0) left = grid[m][n] + bottomUp(grid, dp, m, n-1);
+        
+        return dp[m][n] = min(up, left);
+        
             
     }
     
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        vector<vector<int>> dp(m, vector<int>(n, -1));
         
+//         MEMOIZATION
+        return bottomUp(grid, dp, m-1, n-1);
         
 //         TOP-DOWN APPROACH
         for(int i=0; i<m; i++)
