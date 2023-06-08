64ac81823fd15cf51f18d68cb5f17964de70244b
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2023-06-09 02:09:13 +0530

    Time: 8 ms (15.53%), Space: 7.7 MB (65.77%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -26,7 +26,26 @@ public:
         vector<vector<int>> dp(m, vector<int>(n, -1));
         
 //         MEMOIZATION or BOTTOM-UP APPROACH
-        return bottomUp(obstacleGrid, dp, m-1, n-1);
+        // return bottomUp(obstacleGrid, dp, m-1, n-1);
         
+        
+//         TOP_DOWN APPROACH
+        
+        for(int i=0; i<m; i++)
+            for(int j=0; j<n;j++)
+            { 
+                if(obstacleGrid[i][j] == 1)
+                    dp[i][j] = 0;
+                else if(i==0 && j==0)
+                    dp[0][0] = 1;
+                else
+                {
+                    int right=0, down=0;
+                    if(i>0) right = dp[i-1][j];
+                    if(j>0) down = dp[i][j-1];
+                    dp[i][j] = right + down;
+                }
+            }
+        return dp[m-1][n-1];
     }
 };
