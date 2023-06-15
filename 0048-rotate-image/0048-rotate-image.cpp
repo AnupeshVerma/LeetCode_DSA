c6f04fd3f967b19925223e3423c312cd2378329b
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2023-06-15 23:46:42 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (19.42%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,7 +4,7 @@ public:
         int n = matrix.size();
         
 //         Brute Force Approach
-        vector<vector<int>> ans(n, vector<int>(n));
+        // vector<vector<int>> ans(n, vector<int>(n));
         // for(int i=0; i<n; i++)
         //     for(int j=0; j<n; j++)
         //     {
