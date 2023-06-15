048c431e6c576b84230dc5ff9b32c422589f9d59
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2023-06-15 23:44:31 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (19.42%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,12 +2,22 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
+        
+//         Brute Force Approach
         vector<vector<int>> ans(n, vector<int>(n));
+        // for(int i=0; i<n; i++)
+        //     for(int j=0; j<n; j++)
+        //     {
+        //         ans[i][j] = matrix[n-1-j][i];
+        //     }
+        // matrix = ans;
+        
+        
+//         Optimal Approach
         for(int i=0; i<n; i++)
-            for(int j=0; j<n; j++)
-            {
-                ans[i][j] = matrix[n-1-j][i];
-            }
-        matrix = ans;
+            for(int j=0; j<i; j++)
+                swap(matrix[i][j], matrix[j][i]);
+        for(int i=0; i<n; i++)
+            reverse(matrix[i].begin(), matrix[i].end());
     }
 };
