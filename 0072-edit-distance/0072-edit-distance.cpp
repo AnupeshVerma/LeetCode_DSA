72cff7f008d22aca9cb6f6ebfc9d91ffd93f3c17
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2023-08-01 11:37:03 +0530

    Time: 7 ms (93.24%), Space: 8.9 MB (61.06%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    private:
+    int lcs(string s1, string s2, int n, int m)
+    {
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+        
+        for (int i = 1; i <= n; i++)
+            dp[i][0] = i; 
+
+        for (int j = 1; j <= m; j++)
+            dp[0][j] = j; 
+        
+        for(int i=1; i<=n; i++)
+             for(int j=1; j<=m; j++)
+                 if(s1[i-1] == s2[j-1])
+                     dp[i][j] = dp[i-1][j-1];
+                 else
+                     dp[i][j] = 1+min(min(dp[i-1][j-1], dp[i-1][j]), dp[i][j-1]);
+        return dp[n][m];
+        
+    }
+public:
+    int minDistance(string word1, string word2) {
+        
+        int n = word1.size();
+        int m = word2.size();
+        
+        return lcs(word1, word2, n, m);
+    }
+};
