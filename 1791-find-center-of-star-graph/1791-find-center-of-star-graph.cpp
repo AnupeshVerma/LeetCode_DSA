372955b7d9217791b307808ecc740115be6e8855
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2023-08-01 14:17:48 +0530

    Time: 139 ms (99.51%), Space: 67.2 MB (65.96%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        int n = edges.size();
-        unordered_map<int,int>mp;
+//         int n = edges.size();
+//         unordered_map<int,int>mp;
         
-        for(auto i:edges)
-            for(auto j:i)
-            {
-                mp[j]++;
-            }
-        int ans, freq=0;
-        for(auto it:mp)
-        {
-            cout<<it.second;
-            if(it.second > freq)
-            {
-                ans = it.first;
-                freq = it.second;
-            }
-        }
-        return ans;
+//         for(auto i:edges)
+//             for(auto j:i)
+//             {
+//                 mp[j]++;
+//             }
+//         int ans, freq=0;
+//         for(auto it:mp)
+//         {
+//             cout<<it.second;
+//             if(it.second > freq)
+//             {
+//                 ans = it.first;
+//                 freq = it.second;
+//             }
+//         }
+//         return ans;
+        
+//         Since n-1 nodes are connected to center, there will be no other nodes connected
+//         We compare first four only
+//         Phle vectorke dono me se hi ek center hoga
+//         Dono me konsa node dusre vector me present h whi center h
+        if(edges[0][0]==edges[1][0] || edges[0][0]==edges[1][1])
+            return edges[0][0];
+        else
+            return edges[0][1];
     }
 };
