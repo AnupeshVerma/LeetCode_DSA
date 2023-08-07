f4bcb5dbe0681f47c8703a4f83814cab482ab59d
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2023-08-07 18:18:38 +0530

    Time: 932 ms (54.86%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -6,5 +6,11 @@
 # weather AS w2
 # WHERE w1.temperature > w2.temperature and w1.recordDate = w2.recordDate + interval 1 day;
 
-SELECT w1.id FROM weather w1, weather w2
-WHERE w1.temperature>w2.temperature AND datediff(w1.recordDate, w2.recordDate)=1
+# SELECT w1.id FROM weather w1, weather w2
+# WHERE w1.temperature>w2.temperature AND datediff(w1.recordDate, w2.recordDate)=1
+
+
+SELECT w1.id FROM weather AS w1
+JOIN
+weather AS w2 on datediff(w1.recordDate, w2.recordDate)=1 
+WHERE w1.temperature > w2.temperature;
