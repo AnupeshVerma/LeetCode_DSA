b58110838a99a555df29323104106d3ec339a432
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2023-08-10 23:23:15 +0530

    Time: 340 ms (82.63%), Space: 122.2 MB (35.75%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int minReorder(int n, vector<vector<int>>& connections) {
-        
+    int methodOne(int n, vector<vector<int>>& connections)
+    {
         vector<int>adj[n];
         set<vector<int>>st;
         
@@ -13,6 +13,7 @@ public:
         }
         vector<int>visited(n, 0);
         queue<int>q;
+        
         q.push(0);
         visited[0] = 1;
 
@@ -35,5 +36,48 @@ public:
             }
         }
         return count;
+    }
+    
+    int methodTwo(int n, vector<vector<int>>connections)
+    {
+        vector<pair<int, int>>adj[n];
+        for(auto i:connections)
+        {
+            adj[i[0]].push_back({i[1], 1});
+            adj[i[1]].push_back({i[0], 0});
+        }
+        
+        vector<int>visited(n, 0);
+        queue<int>q;
+        
+        visited[0]=1;
+        q.push(0);
+        
+        int ans = 0;
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+           
+            for(auto [adjNode, adjwt] : adj[node])
+            {
+                if(!visited[adjNode])
+                {
+                    visited[adjNode] = 1;
+                    q.push(adjNode);
+                    ans+=adjwt;
+                }
+            }
+            
+        }
+        return ans;
+    }
+    int minReorder(int n, vector<vector<int>>& connections) {
+        
+//        Using set to store original edges
+        // return methodOne(n, connections);
+        
+//         Assigning weights to backward and forward edges
+        return methodTwo(n, connections);
     }
 };
