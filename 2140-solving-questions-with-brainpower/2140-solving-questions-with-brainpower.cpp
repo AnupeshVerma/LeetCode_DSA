d4d541c3a7914891486501f3c88bed30d474f9ae
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2023-08-11 11:23:44 +0530

    Time: 274 ms (91.85%), Space: 123 MB (41.11%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long solve(vector<vector<int>>&qns, vector<long long>&dp, int ind)
+    {
+        if(ind >= qns.size())
+            return 0;
+        if(dp[ind] != -1)
+            return dp[ind];
+        
+//         Solved
+        long long solved = qns[ind][0] + solve(qns, dp, ind+qns[ind][1]+1);
+        long long skipped= solve(qns, dp, ind+1);
+        
+        return dp[ind] = max(solved, skipped);
+    }
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n = questions.size();
+        vector<long long>dp(n+1, -1);
+        
+        return solve(questions, dp, 0);
+    }
+};
