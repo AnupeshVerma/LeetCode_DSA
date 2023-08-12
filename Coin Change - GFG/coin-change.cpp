fb1a09c048676e5875ee7bcd0146d262edc01d0b
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2023-08-12 10:15:18 +0530

    Added solution - LeetHub

---
+++ Coin Change - GFG/coin-change.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+  long long int solve(int amount, int coins[], vector<vector<long long int>>&dp, int n)
+    {
+        if(amount == 0)
+            return 1;
+        else if(n == 0 || amount<0)
+            return 0;
+        
+        if(dp[n][amount] != -1)
+            return dp[n][amount];
+        
+        if(coins[n-1] <= amount)
+            return dp[n][amount] = solve(amount, coins, dp, n-1) + solve(amount-coins[n-1], coins, dp, n);
+        else
+            return dp[n][amount] = solve(amount, coins, dp, n-1);
+    }
+    long long int count(int coins[], int n, int amount) {
+
+        vector<vector<long long int>>dp(n+1, vector<long long int>(amount+1, -1));
+//         MEMOIZATION
+        return solve(amount, coins, dp, n);
+        
+        
+//         TOP-DOWN
+        for(int i=0; i<n+1; i++)
+            for(int j=0; j<amount+1; j++)
+            {
+                dp[0][j] = 0;
+                dp[i][j] = 1;
+            }
+        
+        for(int i=1; i<n+1; i++)
+            for(int j=1; j<amount+1; j++)
+            {
+                if(coins[i-1] <= j)
+                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+                
+        return dp[n][amount];   
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int sum, N;
+        cin >> sum >> N;
+        int coins[N];
+        for (int i = 0; i < N; i++) cin >> coins[i];
+        Solution ob;
+        cout << ob.count(coins, N, sum) << endl;
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
