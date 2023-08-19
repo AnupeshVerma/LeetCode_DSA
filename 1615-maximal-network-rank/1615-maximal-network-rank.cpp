78eddc5a786f22e32e9b25fba65c3f27e78e3520
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2023-08-20 00:00:37 +0530

    Time: 80 ms (72.71%), Space: 33.8 MB (50.59%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -3,26 +3,46 @@ public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
         vector<int>degree(n, 0);
         
-        set<pair<int, int>>st;
+//         Method 1
+//         set<pair<int, int>>st;
+//         for(auto it:roads)
+//         {
+//             st.insert({it[0], it[1]});
+//             st.insert({it[1], it[0]});
+//             degree[it[0]]++;
+//             degree[it[1]]++;
+//         }
+        
+//         int ans = INT_MIN;
+//         for(int i=0; i<n; i++)
+//             for(int j=i+1; j<n; j++)
+//             {
+//                 int sum_degree = degree[i] + degree[j];
+//                 if(st.find({i, j}) != st.end())
+//                     ans = max(ans, sum_degree-1);
+//                 else
+//                     ans = max(ans, sum_degree);
+//             }
+//         return ans;
         
         
-        for(auto it:roads)
+//         Method 2
+        vector<int>adj[n];
+        
+        for(auto it : roads)
         {
-            st.insert({it[0], it[1]});
-            st.insert({it[1], it[0]});
-            degree[it[0]]++;
-            degree[it[1]]++;
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
         
         int ans = INT_MIN;
         for(int i=0; i<n; i++)
             for(int j=i+1; j<n; j++)
             {
-                int sum_degree = degree[i] + degree[j];
-                if(st.find({i, j}) != st.end())
-                    ans = max(ans, sum_degree-1);
-                else
-                    ans = max(ans, sum_degree);
+                int rank = adj[i].size() + adj[j].size();
+                if(find(adj[i].begin(), adj[i].end(), j) != adj[i].end())
+                    rank--;
+                ans = max(rank, ans);
             }
         return ans;
     }
