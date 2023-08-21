4bb20419152e23a7935f041ba56b137b74c3b36a
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2023-08-21 11:07:00 +0530

    Time: 4 ms (35.22%), Space: 6 MB (62.46%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans=0;
+        
+        for (int i = 0; i < 31; i++)
+        {
+            if(n & 1)
+                ans++;
+            ans<<=1;
+            n>>=1;
+        }
+        if(n & 1)
+            ans++;
+        return ans;
+    }
+};
