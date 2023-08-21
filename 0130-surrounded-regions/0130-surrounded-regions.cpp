610ddeafde7bfa8e12a946720fe72bd77bfada6b
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2023-08-21 12:35:18 +0530

    Time: 7 ms (95.79%), Space: 10.5 MB (20.78%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -7,17 +7,15 @@ public:
         vector<vector<int>>visited(n, vector<int>(m, 0));
         queue<pair<int, int>>q;
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++)
                 if((i==0 || i==n-1 || j==0 || j==m-1) && board[i][j]=='O')
                 {
                     cout<<i<<j<<" ";
                     q.push({i, j});
                     visited[i][j] = 1;
                 }
-            }
-        }
-        
+                
         while(!q.empty())
         {
             auto [row, col] = q.front();
@@ -31,22 +29,19 @@ public:
                 int newRow = row + drow[i];
                 int newCol = col + dcol[i];
                 
-                if(newRow>=0 && newCol>=0 && newRow<n && newCol<m){
+                if(newRow>=0 && newCol>=0 && newRow<n && newCol<m)
                     if(!visited[newRow][newCol] && board[newRow][newCol]=='O')
                     {
                         q.push({newRow, newCol});
                         visited[newRow][newCol] = 1;
                     }
                 }
-            }
         }
-            for(int i=0; i<n; i++){
-                for(int j=0; j<m; j++){
-                    if(!visited[i][j] && board[i][j]=='O')
-                        board[i][j] = 'X';
-                }
-        }
-        }
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++)
+                if(!visited[i][j] && board[i][j]=='O')
+                    board[i][j] = 'X';
+    }
     
     
 };
