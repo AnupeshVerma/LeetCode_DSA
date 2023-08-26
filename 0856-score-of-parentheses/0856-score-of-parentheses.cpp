9d32dbc06a01ccb9ce5d1d43774d7172803369e6
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2023-08-27 02:37:14 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (37.10%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.cpp
+++ 0856-score-of-parentheses/0856-score-of-parentheses.cpp
@@ -1,18 +1,46 @@
 class Solution {
 public:
     int scoreOfParentheses(string s) {
-        int open = 0, score=0;
-        for(int i=0; i<s.size(); i++)
+        // int open = 0, score=0;
+        // for(int i=0; i<s.size(); i++)
+        // {
+        //     if(s[i] == '(')
+        //         open++;
+        //     else
+        //     {
+        //         open--;
+        //         if(s[i-1] == '(')
+        //             score += pow(2, open);
+        //     }
+        // }
+        // return score;
+        
+        stack<int> st;
+        int score = 0;
+        
+        for(auto it:s)
         {
-            if(s[i] == '(')
-                open++;
+            int val = 0;
+            if(it == '(')
+                st.push(0);
             else
             {
-                open--;
-                if(s[i-1] == '(')
-                    score += pow(2, open);
+                while(st.top() != 0)
+                {
+                    val += st.top();
+                    st.pop();
+                }
+                val = max(2*val, 1);
+                st.pop();
+                st.push(val);
             }
         }
+        while(!st.empty())
+        {
+            score += st.top();
+            st.pop();
+        }
+        
         return score;
     }
 };
