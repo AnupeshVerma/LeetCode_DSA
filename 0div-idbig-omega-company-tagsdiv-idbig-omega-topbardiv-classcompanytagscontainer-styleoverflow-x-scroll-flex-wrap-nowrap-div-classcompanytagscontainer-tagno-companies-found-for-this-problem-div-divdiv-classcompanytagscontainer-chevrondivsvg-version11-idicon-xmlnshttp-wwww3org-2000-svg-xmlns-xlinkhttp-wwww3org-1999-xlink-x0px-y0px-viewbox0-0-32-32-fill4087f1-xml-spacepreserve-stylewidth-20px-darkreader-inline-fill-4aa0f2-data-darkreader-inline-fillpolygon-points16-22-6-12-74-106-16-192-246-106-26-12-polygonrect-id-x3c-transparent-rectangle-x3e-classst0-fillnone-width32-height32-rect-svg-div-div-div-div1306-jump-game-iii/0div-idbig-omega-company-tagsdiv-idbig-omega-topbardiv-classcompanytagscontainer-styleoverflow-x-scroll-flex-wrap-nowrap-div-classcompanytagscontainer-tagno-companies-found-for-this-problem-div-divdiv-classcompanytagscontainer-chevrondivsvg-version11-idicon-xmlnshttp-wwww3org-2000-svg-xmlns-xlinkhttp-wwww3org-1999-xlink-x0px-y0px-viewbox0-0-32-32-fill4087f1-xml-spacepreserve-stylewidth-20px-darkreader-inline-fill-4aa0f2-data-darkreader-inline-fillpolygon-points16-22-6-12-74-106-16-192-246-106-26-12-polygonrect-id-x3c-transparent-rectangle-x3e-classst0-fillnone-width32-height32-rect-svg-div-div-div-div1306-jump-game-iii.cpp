ab4dc7b61ab08a2303c24c5d31db4c666c373673
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2024-01-20 19:52:56 +0530

    Time: 36 ms (63.80%), Space: 36.3 MB (40.93%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1306-jump-game-iii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1306-jump-game-iii.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1306-jump-game-iii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1306-jump-game-iii.cpp
@@ -4,7 +4,7 @@ public:
     {
         if(start<0 || start>n-1)
             return false;
-        if(visited[start] ==1)
+        if(visited[start] == 1)
             return false;
         
         if(arr[start] == 0)
@@ -17,9 +17,44 @@ public:
         
         return forward || backward;
     }
+    
+//     -------------------> Using Queue <-------------
+    bool usingQueue(vector<int>& arr, int start)
+    {
+        int n = arr.size();
+        queue<int>q;
+        vector<int> visited(n, 0);
+        
+        q.push(start);
+        while(!q.empty())
+        {
+            int currPosition = q.front();
+            q.pop();
+            
+            if(arr[currPosition] == 0)
+                return true;
+            
+            if(!visited[currPosition])
+            {
+                visited[currPosition] = 1;
+                if(currPosition - arr[currPosition] >= 0)
+                    q.push(currPosition-arr[currPosition]);
+                
+                if(currPosition + arr[currPosition] <= n-1)
+                    q.push(currPosition + arr[currPosition]);
+            }
+            
+        }
+        return false;
+    }
+    
+    
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size();
         vector<int>visited(n, 0);
-        return reach(arr, start, n, visited);
+        // return reach(arr, start, n, visited);
+        
+        
+        return usingQueue(arr, start);
     }
 };
