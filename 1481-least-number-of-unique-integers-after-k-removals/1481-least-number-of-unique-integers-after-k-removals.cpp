cebe68a6579b796582a8110797de9fd2a2d48023
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2024-02-16 13:08:17 +0530

    Time: 126 ms (70.92%), Space: 66.1 MB (51.33%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -23,10 +23,39 @@ private:
     }
     
     
+    int priorityQueue(vector<int>&arr, int k)
+    {
+        unordered_map<int, int>freq;
+        
+        for(auto num:arr)
+            freq[num]++;
+        
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(auto i:freq)
+            pq.push(i.second);
+        
+        int removed = 0;
+        while(k>=0 && !pq.empty())
+        {
+            if(k >= pq.top())
+            {
+                k -= pq.top();
+                removed++;
+                pq.pop();
+            }
+            else
+                break;
+        }
+        
+        return freq.size()-removed;
+    }
+    
 public:
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
         
+        // return bruteForce(arr, k);
         
-        return bruteForce(arr, k);
+        return priorityQueue(arr, k);
+        
     }
 };
