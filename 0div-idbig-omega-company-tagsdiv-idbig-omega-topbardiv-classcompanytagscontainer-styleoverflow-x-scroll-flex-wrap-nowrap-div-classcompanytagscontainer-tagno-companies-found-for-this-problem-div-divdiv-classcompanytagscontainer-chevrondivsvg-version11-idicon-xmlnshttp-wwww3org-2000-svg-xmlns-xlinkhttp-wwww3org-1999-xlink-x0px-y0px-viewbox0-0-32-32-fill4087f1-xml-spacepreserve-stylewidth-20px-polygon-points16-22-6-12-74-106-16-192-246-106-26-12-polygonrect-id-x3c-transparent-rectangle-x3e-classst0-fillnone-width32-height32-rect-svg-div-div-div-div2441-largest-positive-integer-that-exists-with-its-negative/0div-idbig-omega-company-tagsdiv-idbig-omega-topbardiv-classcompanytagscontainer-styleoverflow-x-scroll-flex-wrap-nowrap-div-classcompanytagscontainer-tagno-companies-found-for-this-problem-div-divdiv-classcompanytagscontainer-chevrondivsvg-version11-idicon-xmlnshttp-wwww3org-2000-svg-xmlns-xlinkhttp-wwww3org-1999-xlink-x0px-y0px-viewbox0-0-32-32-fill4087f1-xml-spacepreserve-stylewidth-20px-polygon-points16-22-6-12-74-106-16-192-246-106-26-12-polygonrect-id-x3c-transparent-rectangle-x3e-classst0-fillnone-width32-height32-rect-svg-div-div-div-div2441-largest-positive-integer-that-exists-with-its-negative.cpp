8a71bc6169cb01b5bef777f83eab27dbf778bc17
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2024-05-03 01:26:32 +0530

    Time: 43 ms (8.42%), Space: 22.4 MB (90.47%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2441-largest-positive-integer-that-exists-with-its-negative/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2441-largest-positive-integer-that-exists-with-its-negative.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2441-largest-positive-integer-that-exists-with-its-negative/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2441-largest-positive-integer-that-exists-with-its-negative.cpp
@@ -2,13 +2,22 @@ class Solution {
 private:
     int bruteForce(vector<int>& nums, int n)
     {
+        int ans = -1;
+        for(int i=0; i<n; i++)
+            for(int j=0; j<n; j++)
+                if(nums[i] == -nums[j])
+                    ans = max(ans, abs(nums[i]));
+        return ans;
+    }
+    int usingSet(vector<int>& nums, int n)
+    {
         unordered_set<int> st;
         for(auto num : nums)
             st.insert(num);
         
         sort(nums.begin(), nums.end());
         for(int i=n-1; i>=0; i--)
-            if(nums[i]>0 and st.find(-1*nums[i]) != st.end())
+            if(nums[i]>0 and st.find(-nums[i]) != st.end())
                 return nums[i];
         
         return -1;
@@ -23,7 +32,7 @@ private:
         {
             if(nums[left] == -1*nums[right])
                 return nums[right];
-            else if(nums[left] < -1*nums[right])
+            else if(nums[left] < -nums[right])
                 left++;
             else
                 right--;
@@ -34,7 +43,9 @@ public:
     int findMaxK(vector<int>& nums) {
         int n = nums.size();
         
-        // return bruteForce(nums, n);
+        return bruteForce(nums, n);
+        
+        // return usingSet(nums, n);
         
         return twoPointer(nums, n);
     }
