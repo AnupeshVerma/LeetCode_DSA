e2319829d3660e4ec770691976becd52c450ab51
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2024-07-12 20:34:20 +0530

    Time: 69 ms (82.93%), Space: 18.2 MB (75.61%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1717-maximum-score-from-removing-substrings/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1717-maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        int totalScore = 0;
+        
+        if(x > y)
+        {
+            // First remove "ab" then "ba" --> 'ab' has high priority
+            totalScore += removeSubstring(s, "ab", x);
+            totalScore += removeSubstring(s, "ba", y);
+
+        }
+        else
+        {
+            // First remove "ba" then "ab" --> 'ba' has high priority
+            totalScore += removeSubstring(s, "ba", y);
+            totalScore += removeSubstring(s, "ab", x);
+
+        }
+        return totalScore;
+    }
+    
+    int removeSubstring(string& s, string toRemove, int points)
+    {
+        int score = 0;
+        int j = 0;
+        
+        for(int i=0; i<s.size(); i++)
+        {
+            s[j++] = s[i];
+            
+            if(j>1 && s[j-2]==toRemove[0] && s[j-1]==toRemove[1])
+            {
+                j -=2;
+                score += points;
+                
+            }
+        }
+         s.erase(s.begin() + j, s.end());
+        return score;
+    }
+};
