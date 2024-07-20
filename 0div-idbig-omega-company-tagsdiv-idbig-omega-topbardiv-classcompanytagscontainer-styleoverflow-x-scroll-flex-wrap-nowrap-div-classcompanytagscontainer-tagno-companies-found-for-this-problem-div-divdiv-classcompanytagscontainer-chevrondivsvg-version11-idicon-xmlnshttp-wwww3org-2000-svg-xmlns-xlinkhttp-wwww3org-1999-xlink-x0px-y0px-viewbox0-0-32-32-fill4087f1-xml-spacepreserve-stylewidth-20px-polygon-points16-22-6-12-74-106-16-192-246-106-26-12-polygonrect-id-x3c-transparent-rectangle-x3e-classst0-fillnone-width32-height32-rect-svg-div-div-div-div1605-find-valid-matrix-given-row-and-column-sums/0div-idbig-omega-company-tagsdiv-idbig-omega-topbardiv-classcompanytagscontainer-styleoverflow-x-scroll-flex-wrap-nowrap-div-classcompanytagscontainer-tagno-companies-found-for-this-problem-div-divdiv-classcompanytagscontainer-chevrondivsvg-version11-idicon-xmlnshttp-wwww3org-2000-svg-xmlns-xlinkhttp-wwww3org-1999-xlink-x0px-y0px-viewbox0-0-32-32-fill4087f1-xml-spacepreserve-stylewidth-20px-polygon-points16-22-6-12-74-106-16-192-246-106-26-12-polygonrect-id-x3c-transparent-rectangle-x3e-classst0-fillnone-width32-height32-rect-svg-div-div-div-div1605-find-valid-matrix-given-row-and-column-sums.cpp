ec20a026fa0817cfd909b600a7ed172ee38d43f3
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2024-07-20 23:46:21 +0530

    Time: 33 ms (82.62%), Space: 36.3 MB (6.09%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1605-find-valid-matrix-given-row-and-column-sums/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1605-find-valid-matrix-given-row-and-column-sums.cpp
@@ -0,0 +1,51 @@
+class Solution {
+private:
+    vector<vector<int>> greedy(vector<int>& rowSum, vector<int>& colSum, int n, int m)
+    {
+        vector<vector<int>> ans(n, vector<int>(m, 0));
+        vector<int> currRowSum(n, 0);
+        vector<int> currColSum(m, 0);
+        
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                ans[i][j] = min(rowSum[i]-currRowSum[i], colSum[j]-currColSum[j]);
+                
+                currRowSum[i] += ans[i][j];
+                currColSum[j] += ans[i][j];
+            }
+        }
+        return ans;
+    }
+    
+    vector<vector<int>> optimized(vector<int> rowSum, vector<int> colSum, int n, int m)
+    {
+        vector<vector<int>> ans(n, vector<int>(m, 0));
+        
+        int i=0, j=0;
+        while(i<n && j<m)
+        {
+            ans[i][j] = min(rowSum[i], colSum[j]);
+            
+            rowSum[i] -= ans[i][j];
+            colSum[j] -= ans[i][j];
+            
+            rowSum[i]==0 ? i++ : j++;
+        }
+        return ans;
+    }
+    
+    
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        int n = rowSum.size();
+        int m = colSum.size();
+        
+        // Greedy approach --> O(nxm)
+        return greedy(rowSum, colSum, n, m);
+        
+        // Time optimized --> O(n+m)
+        return optimized(rowSum, colSum, n, m);
+    }
+};
