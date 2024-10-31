82f9c69ed74ded81922e8c0064f916ff1655cbff
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2024-10-31 11:42:23 +0530

    Time: 4 ms (92.38%), Space: 74.4 MB (52.67%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -1,49 +1,38 @@
 class Solution {
+private:
+    int TOTAL_ROW, TOTAL_COL;
+    int topDownDP(vector<vector<int>>& grid, int row, int col, vector<vector<int>>& dp) {
+        if(row >= TOTAL_ROW || col >= TOTAL_COL)
+            return 0;
+        
+        if(dp[row][col] != -1)
+            return dp[row][col];
+
+        int right=0, upper_diagonal=0, lower_diagonal=0;        
+        if(row-1 >= 0 && col+1 < TOTAL_COL &&  grid[row][col] < grid[row-1][col+1])
+            upper_diagonal = 1 + topDownDP(grid, row-1, col+1, dp);
+
+        if(col+1 < TOTAL_COL && grid[row][col] < grid[row][col+1])
+            right = 1 + topDownDP(grid, row, col+1, dp);
+
+        if(row+1 < TOTAL_ROW && col+1 < TOTAL_COL && grid[row][col] < grid[row+1][col+1])
+            lower_diagonal = 1 + topDownDP(grid, row+1, col+1, dp);
+
+        return dp[row][col] = max(right, max(upper_diagonal, lower_diagonal));
+    }
+
+
 public:
-    // The three possible directions for the next column.
-    const int dirs[3] = {-1, 0, 1};
-
     int maxMoves(vector<vector<int>>& grid) {
-        int M = grid.size(), N = grid[0].size();
+        int ans = 0;
+        TOTAL_ROW = grid.size();
+        TOTAL_COL = grid[0].size();
 
-        queue<vector<int>> q;
-        vector<vector<int>> vis(M, vector<int>(N, 0));
-        // Enqueue the cells in the first column.
-        for (int i = 0; i < M; i++) {
-            vis[i][0] = 1;
-            q.push({i, 0, 0});
-        }
+        vector<vector<int>> dp(TOTAL_ROW, vector<int>(TOTAL_COL, -1));
 
-        int maxMoves = 0;
-        while (!q.empty()) {
-            int sz = q.size();
-
-            while (sz--) {
-                vector<int> v = q.front();
-                q.pop();
-
-                // Current cell with the number of moves made so far.
-                int row = v[0], col = v[1], count = v[2];
-
-                maxMoves = max(maxMoves, count);
-
-                for (int dir : dirs) {
-                    // Next cell after the move.
-                    int newRow = row + dir, newCol = col + 1;
-
-                    // If the next cell isn't visited yet and is greater than
-                    // the current cell value. Add it to the queue with the
-                    // moves required.
-                    if (newRow >= 0 && newCol >= 0 && newRow < M &&
-                        newCol < N && !vis[newRow][newCol] &&
-                        grid[row][col] < grid[newRow][newCol]) {
-                        vis[newRow][newCol] = 1;
-                        q.push({newRow, newCol, count + 1});
-                    }
-                }
-            }
-        }
-
-        return maxMoves;
+        for(int row=0; row<TOTAL_ROW; row++)
+            ans = max(ans, topDownDP(grid, row, 0, dp));
+        
+        return ans;
     }
 };
