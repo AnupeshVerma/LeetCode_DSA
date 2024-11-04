117684e77a5677a68d93dc7206d3f7429930bdc6
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2024-11-05 02:39:14 +0530

    Time: 59 ms (20.81%), Space: 33.3 MB (5.19%) - LeetHub

---
+++ 3163-string-compression-iii/3163-string-compression-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    string bruteForce(string word, int n) {
+        string comp = "";
+        
+        int count = 0;
+        for(int i=0; i<n-1; i++) {
+            count++;
+            if (count == 9 || word[i] != word[i+1]) {
+                comp += to_string(count);
+                comp += word[i];
+                count = 0;
+            }
+        }
+            comp += to_string(count+1);
+            comp += word[n-1];
+        
+        return comp;
+    }
+    
+    
+public:
+    string compressedString(string word) {
+        int n = word.size();
+        
+        return bruteForce(word, n);
+    }
+};
