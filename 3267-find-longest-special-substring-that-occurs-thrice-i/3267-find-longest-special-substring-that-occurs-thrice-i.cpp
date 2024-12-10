2be9d0748693986608bfb4d6d5d7de28232e8815
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2024-12-10 23:48:24 +0530

    Time: 83 ms (19.83%), Space: 32 MB (16.91%) - LeetHub

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -0,0 +1,39 @@
+class Solution {
+private:
+    bool isSpecial(string s) {
+        char a = s[0];
+        for(char c : s)
+            if(c != a)
+                return false;
+        return true;
+    }
+
+
+    int bruteForce(string s, int n) {
+        unordered_map<string, int> freq;
+
+        for(int i=0; i<n; i++) {
+            string temp = "";
+            for(int j=i; j<n; j++) {
+                temp += s[j];
+                if(isSpecial(temp))
+                    freq[temp]++;
+            }
+        }
+
+        int ans = -1;
+        for(auto it : freq){
+            if(it.second >= 3) {
+                int size = it.first.size();
+                ans = max(ans, size);
+            }
+        }
+        return ans;
+    }
+public:
+    int maximumLength(string s) {
+        int n = s.size();
+
+        return bruteForce(s, n);
+    }
+};
