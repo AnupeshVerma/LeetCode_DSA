171ff84ffa515fbfbf5363ee41c0dc1921bd9af9
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2024-12-14 00:09:58 +0530

    Time: 1063 ms (7.77%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,9 +1,25 @@
+##### METHOD 1 #####
+
+-- SELECT
+--     LEFT(trans_date, 7) AS 'month',
+--     country,
+--     COUNT(*) AS trans_count,
+--     SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+--     SUM(amount) AS trans_total_amount,
+--     SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+-- FROM Transactions
+-- GROUP BY LEFT(trans_date, 7), country
+
+
+
+##### METHOD 2 #####
+
 SELECT
     LEFT(trans_date, 7) AS 'month',
     country,
     COUNT(*) AS trans_count,
-    SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+    SUM(state = 'approved') AS approved_count,
     SUM(amount) AS trans_total_amount,
-    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+    SUM((state = 'approved') * amount) AS approved_total_amount
 FROM Transactions
 GROUP BY LEFT(trans_date, 7), country
