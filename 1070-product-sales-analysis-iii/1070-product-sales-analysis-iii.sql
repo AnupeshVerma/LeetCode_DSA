64efa3df7bdfb120941a1fed9ca26efcc0862a28
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2024-12-16 21:01:43 +0530

    Time: 1365 ms (20.84%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,11 +1,32 @@
-SELECT 
-    product_id,
-    year AS first_year,
-    quantity,
-    price
-FROM Sales AS s
-WHERE year = (
-    SELECT MIN(year)
+##### METHOD 1 (Gives TLE) ######
+
+-- SELECT 
+--     product_id,
+--     year AS first_year,
+--     quantity,
+--     price
+-- FROM Sales AS s
+-- WHERE year = (
+--     SELECT MIN(year)
+--     FROM Sales
+--     WHERE product_id = s.product_id
+-- )
+
+
+
+##### METHOD 2 ######
+
+WITH sortedYear AS (
+    SELECT product_id, MIN(year) AS min_year
     FROM Sales
-    WHERE product_id = s.product_id
+    GROUP BY product_id
 )
+
+SELECT
+    s.product_id,
+    s.year AS first_year,
+    s.quantity,
+    s.price
+FROM Sales AS s
+JOIN sortedYear AS sy
+ON s.product_id = sy.product_id AND s.year = sy.min_year
