35a3f2c92a6b6199c6f48d07cccb1317ebe47365
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2025-02-24 15:04:07 +0530

    Time: 442 ms (88.45%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,5 +1,5 @@
 -- Using Subqueries
-/*
+
 SELECT s.name
 FROM SalesPerson AS s
 WHERE s.sales_id NOT IN (
@@ -7,7 +7,7 @@ WHERE s.sales_id NOT IN (
         WHERE com_id = (SELECT com_id FROM Company WHERE name = 'RED')
 );
 
-*/
+
 
 
 -- Using JOIN
@@ -17,7 +17,7 @@ WHERE s.sales_id NOT IN (
 
 
 -- Using Common Table Expression
-
+/*
 
 WITH acceptable_sales_ids AS (
     SELECT sales_id FROM Orders
@@ -28,3 +28,4 @@ SELECT s.name
 FROM SalesPerson AS s
 WHERE s.sales_id NOT IN (SELECT sales_id FROM acceptable_sales_ids);
 
+*/
