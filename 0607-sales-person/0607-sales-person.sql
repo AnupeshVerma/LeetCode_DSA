e581b59b58255a614e41746be98e6d83b1eced03
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2025-02-24 15:07:10 +0530

    Time: 541 ms (75.04%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,5 +1,5 @@
 -- Using Subqueries
-
+/*
 SELECT s.name
 FROM SalesPerson AS s
 WHERE s.sales_id NOT IN (
@@ -7,12 +7,19 @@ WHERE s.sales_id NOT IN (
         WHERE com_id = (SELECT com_id FROM Company WHERE name = 'RED')
 );
 
-
+*/
 
 
 -- Using JOIN
 
-
+SELECT s.name
+FROM SalesPerson AS s
+WHERE s.sales_id NOT IN (
+        SELECT sales_id FROM Orders
+        JOIN Company AS c
+        USING (com_id)
+        WHERE c.name = 'RED'
+);
 
 
 
