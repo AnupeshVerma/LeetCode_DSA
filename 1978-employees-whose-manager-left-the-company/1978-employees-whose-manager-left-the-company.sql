f36829fe884ac41911934611a745a213330047da
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2025-02-24 12:46:07 +0530

    Time: 325 ms (71.98%), Space: 0B (100%) - LeetHub

--- 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -1,4 +1,6 @@
 SELECT employee_id
 FROM Employees
-WHERE salary < 30000 AND manager_id NOT IN (SELECT employee_id FROM Employees)
-ORDER BY employee_id
+WHERE 
+    salary < 30000 AND
+    manager_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY employee_id;
