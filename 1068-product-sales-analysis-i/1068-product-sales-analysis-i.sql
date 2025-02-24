c2f1a83fb8eb1c8f2b9ed05dfe457c99c7a1fef0
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2025-02-24 14:22:52 +0530

    Time: 3445 ms (12.12%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,5 +1,19 @@
+-- Using Subquery
+/*
 SELECT
     (SELECT product_name FROM Product WHERE product_id = s.product_id),
     s.year,
     s.price
 FROM Sales AS s;
+
+*/
+
+
+-- Using JOIN
+SELECT 
+    p.product_name,
+    s.year,
+    s.price
+FROM Sales AS s
+INNER JOIN Product AS p
+USING (product_id)
