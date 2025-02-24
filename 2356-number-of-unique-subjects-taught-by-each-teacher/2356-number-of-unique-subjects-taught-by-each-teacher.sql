2aa2cb97ab2f033f2f806522270b6382d3c7472a
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2025-02-24 15:40:21 +0530

    Time: 1075 ms (31.58%), Space: 0B (100%) - LeetHub

--- 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.sql
+++ 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.sql
@@ -1,8 +1,3 @@
-SELECT teacher_id, COUNT(*) As cnt
-FROM (
-    SELECT teacher_id, subject_id
-    FROM Teacher
-    GROUP BY teacher_id, subject_id
-)
-GROUP BY teacher_id;
-
+SELECT teacher_id, COUNT(DISTINCT subject_id) AS cnt
+FROM Teacher
+GROUP BY teacher_id 
