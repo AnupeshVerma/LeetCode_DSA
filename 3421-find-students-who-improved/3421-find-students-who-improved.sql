09ee5cfd89db9a420344051abd8013ad1053bf03
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2025-02-26 17:53:32 +0530

    Time: 302 ms (67.12%), Space: 0B (100%) - LeetHub

--- 3421-find-students-who-improved/3421-find-students-who-improved.sql
+++ 3421-find-students-who-improved/3421-find-students-who-improved.sql
@@ -30,9 +30,9 @@ JOIN scores l
 ON 
     f.student_id = l.student_id AND 
     f.subject = l.subject AND
-    f.exam_date != l.exam_date AND
+    f.exam_date != l.exam_date  
+WHERE 
+    f.score < l.score AND
     f.exam_date = (SELECT min(exam_date) FROM scores WHERE f.student_id = student_id AND f.subject = subject) AND
     l.exam_date = (SELECT max(exam_date) FROM scores WHERE l.student_id = student_id AND l.subject = subject)
-WHERE 
-    f.score < l.score 
 ORDER BY f.student_id,f.subject;
