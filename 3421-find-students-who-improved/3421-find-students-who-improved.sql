b1b537335410090c5f12da542057bf6c52c21ee0
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2025-02-26 17:52:22 +0530

    Time: 842 ms (5.03%), Space: 0B (100%) - LeetHub

--- 3421-find-students-who-improved/3421-find-students-who-improved.sql
+++ 3421-find-students-who-improved/3421-find-students-who-improved.sql
@@ -1,3 +1,6 @@
+-- Using WINDOW FUNCTIONS
+/*
+
 SELECT DISTINCT
     student_id, 
     subject, 
@@ -11,3 +14,25 @@ FROM (
 ) AS a
 WHERE a.first_score < a.latest_score
 ORDER BY student_id, subject;
+
+*/
+
+
+
+
+SELECT 
+    f.student_id,
+    f.subject,
+    f.score as first_score,
+    l.score as latest_score
+FROM scores AS f 
+JOIN scores l 
+ON 
+    f.student_id = l.student_id AND 
+    f.subject = l.subject AND
+    f.exam_date != l.exam_date AND
+    f.exam_date = (SELECT min(exam_date) FROM scores WHERE f.student_id = student_id AND f.subject = subject) AND
+    l.exam_date = (SELECT max(exam_date) FROM scores WHERE l.student_id = student_id AND l.subject = subject)
+WHERE 
+    f.score < l.score 
+ORDER BY f.student_id,f.subject;
