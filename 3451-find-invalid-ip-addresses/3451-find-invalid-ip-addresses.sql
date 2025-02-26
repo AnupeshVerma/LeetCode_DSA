5ef1f861b26370b7897427dbb7548c8baf545c73
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2025-02-26 20:18:21 +0530

    Time: 360 ms (31.41%), Space: 0B (100%) - LeetHub

--- 3451-find-invalid-ip-addresses/3451-find-invalid-ip-addresses.sql
+++ 3451-find-invalid-ip-addresses/3451-find-invalid-ip-addresses.sql
@@ -2,9 +2,4 @@ SELECT ip, COUNT(*) AS invalid_count
 FROM logs
 WHERE ip !~ '^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?)$'
 GROUP BY ip
-ORDER BY invalid_count DESC, ip DESC
-
-
-
--- WHERE ip !~ '^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]?)$'
-
+ORDER BY invalid_count DESC, ip DESC;
