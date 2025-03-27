311e36fa78d79e11f33491e289007105e7c20bd7
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2025-03-27 17:41:55 +0530

    Time: 189 ms (90.45%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,11 +1,19 @@
+-- -- Write your PostgreSQL query statement below
+-- SELECT 
+--     machine_id,
+--     ROUND(SUM(
+--         CASE WHEN activity_type = 'start' THEN -1*timestamp::INT ELSE timestamp::INT END
+--     )/COUNT(*), 3) AS processing_time
+-- FROM Activity
+-- GROUP BY machine_id
+
+
 SELECT 
     a1.machine_id,
-    ROUND(
-        AVG(a2.timestamp - a1.timestamp), 3) AS processing_time
+    ROUND(AVG(a1.timestamp::NUMERIC - a2.timestamp::NUMERIC), 3) AS processing_time
 FROM Activity a1, Activity a2
 WHERE
     a1.machine_id = a2.machine_id AND
-    a1.process_id = a2.process_id AND
-    a1.activity_type = 'start' AND
-    a2.activity_type = 'end'
-GROUP BY machine_id
+    a1.activity_type = 'end' AND
+    a2.activity_type = 'start'
+GROUP BY a1.machine_id;
