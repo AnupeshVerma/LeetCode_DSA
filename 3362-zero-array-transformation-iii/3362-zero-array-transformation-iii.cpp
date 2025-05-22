1e5b9e11430802205843a88a5f09bd3cde2acf2f
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2025-05-22 21:56:50 +0530

    Time: 215 ms (6.99%), Space: 262 MB (5.06%) - LeetHub

--- 3362-zero-array-transformation-iii/3362-zero-array-transformation-iii.cpp
+++ 3362-zero-array-transformation-iii/3362-zero-array-transformation-iii.cpp
@@ -1,4 +1,37 @@
 class Solution {
+private:
+    int usingPriorityQueue(vector<int> nums, vector<vector<int>> queries, int n, int q) {
+        priority_queue<int> endings;
+        priority_queue<int, vector<int>, greater<int>> pastEndings;
+
+        int j=0;
+        int usedCount = 0;
+        for(int i=0; i<n; i++) {
+            // Put all the endings of queries start from current index
+            while(j < q && queries[j][0] == i) {
+                endings.push(queries[j][1]);
+                j++;
+            }
+
+            // Check if past queries impacted the current i
+            nums[i] -= pastEndings.size();
+            while(nums[i] > 0 && !endings.empty() && endings.top() >= i) {
+                int ending = endings.top();
+                endings.pop();
+                pastEndings.push(ending);
+                usedCount++;
+                nums[i]--;
+            }
+            if(nums[i] > 0)
+                return -1;
+            
+            // Clear the outdated indices
+            while(!pastEndings.empty() && pastEndings.top() <= i)
+                pastEndings.pop();
+        }
+        return q-usedCount;
+    }
+
 public:
     int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
@@ -6,6 +39,9 @@ public:
 
         sort(queries.begin(), queries.end());
 
+        return usingPriorityQueue(nums, queries, n, q);
+        
+        
         priority_queue<int> pq;
         vector<int>deltaArray(n+1, 0);
         int ops = 0;
@@ -20,7 +56,7 @@ public:
 
             while(ops < nums[i] && !pq.empty() && pq.top() >= i) {
                 ops +=1;
-                deltaArray[pq.top() + 1] -=1;
+                deltaArray[pq.top() + 1] -= 1;
                 pq.pop();
             }
             if(ops < nums[i])
