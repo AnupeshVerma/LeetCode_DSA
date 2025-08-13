2fbe949640a8877996f22ade1509c8100089fc48
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2025-08-13 10:42:56 +0530

    Time: 6 ms (27.44%), Space: 8.9 MB (46.41%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -10,8 +10,17 @@ private:
         }
         return false;
     }
+
+    bool simplest(int n) {
+        int maxPowerOfThree = 1162261467; // 3^19 is the largest power of 3 in int range
+        return n > 0 && maxPowerOfThree % n == 0;
+    }
+
 public:
     bool isPowerOfThree(int n) {
-        return bruteForce(n);
+
+        // return bruteForce(n);
+
+        return simplest(n);
     }
 };
