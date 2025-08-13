56b8727e1fbce13c1f587e7f9525835e4a4f0dca
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2025-08-13 10:38:49 +0530

    Time: 1 ms (73.42%), Space: 9.2 MB (5.34%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,10 +1,17 @@
 class Solution {
+private:
+    bool bruteForce(int n) {
+        for(int i=0; i<n; i++){
+            long powerVal = pow(3, i);
+            if(powerVal > n)
+                return false;
+            if(powerVal == n)
+                return true;
+        }
+        return false;
+    }
 public:
     bool isPowerOfThree(int n) {
-       if(n<1)
-           return false;
-        while(n%3 == 0)
-            n/=3;
-        return n == 1;   
+        return bruteForce(n);
     }
 };
