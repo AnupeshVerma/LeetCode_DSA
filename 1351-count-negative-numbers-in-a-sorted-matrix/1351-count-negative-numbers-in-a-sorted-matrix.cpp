a8f38ad4010987d990cde1ff86f784717e98e062
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2025-12-29 00:40:19 +0530

    Time: 0 ms (100%), Space: 13.9 MB (99.63%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -12,11 +12,26 @@ private:
         return count;
     }
 
+    int loopBreak(vector<vector<int>>& grid, int m, int n) {
+        int count = 0;
+
+        for(int row=0; row<m; row++) {
+            for(int col=n-1; col>=0; col--) {
+                if(grid[row][col] >= 0)
+                    break;
+                count++;
+            }
+        }
+        return count;
+    }
+
 public:
     int countNegatives(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
+        return loopBreak(grid, m, n);
+        
         return bruteForce(grid, m, n);
     }
 };
