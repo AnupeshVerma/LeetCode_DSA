f6aaa4aa5980cc79b4e4ae8f327052032d36f060
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2026-03-29 13:21:41 +0530

    Time: 0 ms (100%), Space: 9.4 MB (99.21%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,6 +1,6 @@
 class Solution {
-public:
-    vector<vector<int>> generate(int numRows) {
+private:
+    vector<vector<int>> name1(int numRows) {
         vector<vector<int>> pascal;
 
         // Initialize the triangle with all 1s
@@ -14,5 +14,27 @@ public:
             }
         }
         return pascal;
+    }
+
+    vector<vector<int>> name2(int numRows) {
+        vector<vector<int>> pascal;
+
+        for(int i=0; i<numRows; i++) {
+            vector<int> row(i+1, 1);
+
+            // Update the middle rows
+            for(int j=1; j<i; j++) {
+                row[j] = pascal[i-1][j-1] + pascal[i-1][j];
+            }
+            pascal.push_back(row);
+        }
+
+        return pascal;
+    }
+
+public:
+    vector<vector<int>> generate(int numRows) {
+        
+        return name2(numRows);
     }
 };
