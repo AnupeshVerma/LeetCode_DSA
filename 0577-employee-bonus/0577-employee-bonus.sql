35ffca9d53c2baf4a5585797a7071dfc6c95c1dc
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2026-04-09 23:25:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,3 +1,14 @@
+/*
+
+-- this will not work because NULL < 1000 give unknown
+-- so you have to add this in where condition explicitly
+SELECT e.name, COALESCE(b.bonus, null) AS bonus 
+FROM Employee AS e
+LEFT JOIN Bonus AS b USING (empID)
+WHERE b.bonus < 1000
+
+*/
+
 SELECT 
     e.name,
     b.bonus
