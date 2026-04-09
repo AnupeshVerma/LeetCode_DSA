4ae2e9e9e8b4f52509b7344f5fb9f6b99991c845
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2026-04-09 21:40:22 +0530

    Time: 883 ms (6.48%), Space: 0B (100%) - LeetHub

--- 1683-invalid-tweets/1683-invalid-tweets.sql
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -1,5 +1,2 @@
-# Write your MySQL query statement below
-
-# SELECT tweet_id FROM Tweets WHERE length(content)>15
-
-SELECT tweet_id FROM Tweets WHERE char_length(content)>15
+SELECT tweet_id FROM Tweets
+WHERE LENGTH(content) > 15
