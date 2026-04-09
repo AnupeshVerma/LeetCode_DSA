d68c309562db58bd21d2aada205f9307f9a3a2c7
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2026-04-09 22:16:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1393-capital-gainloss/1393-capital-gainloss.sql
+++ 1393-capital-gainloss/1393-capital-gainloss.sql
@@ -1,5 +1,5 @@
 SELECT 
     stock_name,
-    SUM(CASE WHEN operation = 'Sell' THEN price ELSE 0 END) - SUM(CASE WHEN operation = 'Buy' THEN price ELSE 0 END) AS capital_gain_loss
+    SUM(IF(operation = 'Sell', price, 0)) - SUM(IF(operation = 'Buy', price, 0)) AS capital_gain_loss
 FROM Stocks
 GROUP BY stock_name
