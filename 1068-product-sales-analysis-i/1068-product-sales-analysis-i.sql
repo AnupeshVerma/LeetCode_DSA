5581868a26a2af98f02a4f12b28a03a9e6f8c886
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2026-04-09 20:52:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,19 +1,3 @@
--- Using Subquery
-/*
-SELECT
-    (SELECT product_name FROM Product WHERE product_id = s.product_id),
-    s.year,
-    s.price
-FROM Sales AS s;
-
-*/
-
-
--- Using JOIN
-SELECT 
-    p.product_name,
-    s.year,
-    s.price
-FROM Sales AS s
-INNER JOIN Product AS p
-USING (product_id)
+SELECT product_name, year, price
+FROM Sales
+JOIN Product USING(product_id);
