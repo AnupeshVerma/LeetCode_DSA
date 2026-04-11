f7505ae995c16f8d19ea4450b9a8622db11dd95e
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2026-04-11 23:26:02 +0530

    Time: 255 ms (85.75%), Space: 279.1 MB (88%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
@@ -1,63 +1,51 @@
 class Solution {
 private:
     int n;
-    int min_dist = INT_MAX;
+    int minDist = INT_MAX;
 
-    int usingFlag(vector<int>& nums) {
-        int temp_dist;
-
-        for(int i=0; i<n-2; i++) {
-            int secondOccurrence = false;
-            for(int j=i+1; j<n; j++) {
+    int bruteForce(vector<int>& nums) {
+        for(int i = 0; i < n-2; i++) {
+            bool secondOccurrenceFound = false;
+            for(int j = i+1; j < n; j++) {
                 if(nums[i] == nums[j]) {
-                    // If occurred for the second time
-                    if(!secondOccurrence) {
-                        secondOccurrence = true;
+                    if(!secondOccurrenceFound) {
+                        secondOccurrenceFound = true;
                         continue;
                     }
-
-                    // If occurred more than twice
-                    min_dist = min(min_dist, 2*(j - i));
+                    minDist = min(minDist, 2*(j - i));
                 }
             }
         }
-        return min_dist == INT_MAX ? -1 : min_dist;
+        return minDist == INT_MAX ? -1 : minDist;
     }
 
-    int usingMaps(vector<int>& nums) {
+    int optimized(vector<int>& nums) {
         unordered_map<int, int> firstOccur;
         unordered_map<int, int> secondOccur;
 
-        for(int i=0; i<n; i++) {
-            // if occurred first time, store it
+        for(int i = 0; i < n; i++) {
             if(firstOccur.find(nums[i]) == firstOccur.end()) {
                 firstOccur[nums[i]] = i;
                 continue;
             }
-            // If found second time
             if(secondOccur.find(nums[i]) == secondOccur.end()) {
                 secondOccur[nums[i]] = i;
                 continue;
             }
-
-            // If found more than twice
-            min_dist = min(min_dist, 2*(i - firstOccur[nums[i]]));
-            // Slide window forward
+            minDist = min(minDist, 2*(i - firstOccur[nums[i]]));
             firstOccur[nums[i]] = secondOccur[nums[i]];
             secondOccur[nums[i]] = i;
-            
         }
 
-        return min_dist == INT_MAX ? -1 : min_dist;
+        return minDist == INT_MAX ? -1 : minDist;
     }
-
 
 public:
     int minimumDistance(vector<int>& nums) {
         n = nums.size();
+        
+        return optimized(nums);
 
-        return usingMaps(nums);
-
-        return usingFlag(nums);
+        // return bruteForce(nums);
     }
 };
