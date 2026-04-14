a694872528f20d7e6085291a05a44743ca8e1bdf
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2026-04-14 20:35:34 +0530

    Time: 420 ms (17.12%), Space: 0B (100%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -1,4 +1,6 @@
-SELECT user_id, MAX(time_stamp) AS last_stamp
-FROM Logins
-WHERE YEAR(time_stamp) = 2020
+SELECT 
+    user_id,
+    MAX(time_stamp) AS last_stamp
+FROM logins
+WHERE EXTRACT(YEAR FROM time_stamp) = '2020'
 GROUP BY user_id
