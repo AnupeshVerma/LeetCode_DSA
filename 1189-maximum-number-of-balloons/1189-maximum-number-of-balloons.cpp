627f67ae5b60a7a6958a94e130a6deb6b238a88a
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2026-06-22 06:17:34 +0530

    Time: 0 ms (100%), Space: 8.6 MB (99.49%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int freq[26] = {0};
+
+        for(char c : text) {
+            freq[c - 'a'] += 1;
+        }
+
+        return min({freq['b' - 'a'], freq['a' - 'a'], freq['l' - 'a']/2, freq['o' - 'a']/2, freq['n' - 'a']});
+    }
+};
